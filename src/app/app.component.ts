ce42ad572070df1bd5064faca8ae0f9915a0b3ef
dc-p8 <cardinal.dorian@gmail.com>
2018-11-24 14:18:22 +0100

    identity device

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+
+
+import { Component, ChangeDetectorRef } from '@angular/core';
+
+import { ElectronService } from 'ngx-electron';
+import { Key } from 'protractor';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'launchmad-emulator';
+	midi:WebMidi.MIDIAccess = null;
+	available_devices:Map<String, any> = new Map();
+	buttons = [
+		0, 1, 2, 3, 4, 5, 6, 7, 8,
+		0, 1, 2, 3, 4, 5, 6, 7, 8,
+		0, 1, 2, 3, 4, 5, 6, 7, 8,
+		0, 1, 2, 3, 4, 5, 6, 7, 8,
+		0, 1, 2, 3, 4, 5, 6, 7, 8,
+		0, 1, 2, 3, 4, 5, 6, 7, 8,
+		0, 1, 2, 3, 4, 5, 6, 7, 8,
+	]
+	constructor(private _electronService: ElectronService,
+		private changeDetectorRef:ChangeDetectorRef) {
+
+	}
+
+	array(): any[] {
+		return Array(9*9);
+	  }
+
+	select(device:WebMidi.MIDIInput | WebMidi.MIDIOutput){
+		console.log(device);
+	}
+
+	updateDevices(){
+		if(this.midi)
+		{
+			let inputs = Array.from(this.midi.inputs.values());
+			let outputs = Array.from(this.midi.outputs.values());
+			this.available_devices.clear();
+			var devices = inputs.forEach((input) => {
+				let output = outputs.find((output) => {
+					return output.name == input.name;
+				});
+				if(output != null)
+				{
+					this.available_devices.set(input.name,  {
+						input:input,
+						output:output
+					})
+				}
+			});
+			this.changeDetectorRef.detectChanges();
+			console.log(this.available_devices);
+		}
+	}
+
+	ngOnInit() {
+		navigator.requestMIDIAccess().then((midi) => {
+			this.midi = midi;
+			this.updateDevices();
+			this.midi.onstatechange = () => {
+				this.updateDevices();
+			}
+		})
+	}
+	ngAfterViewInit(){
+		
+	}
+	title = 'launchmad-emlator';
+	devicename = '';
+	
+	onSubmit() {
+		this._electronService.ipcRenderer.send('enable-device', this.devicename);
+	}
+	
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {FormsModule} from '@angular/forms';
+
+import {NgxElectronModule} from 'ngx-electron';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgxElectronModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
